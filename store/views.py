8fef65cec5a68e86812a21d370a7c9bb43088daa
Arash <arash.golrokhian@hotmail.com>
2022-03-09 14:40:21 +0330

    Added Pagination

--- store/views.py
+++ store/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import get_object_or_404
 from django.db.models import Count
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.views import APIView
-from rest_framework.mixins import ListModelMixin
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import status
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from .models import Product, Collection, OrderItem, Review
 from .filter import ProductFilter
@@ -17,8 +13,10 @@ from .filter import ProductFilter
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
