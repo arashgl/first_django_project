8909a6c8e51efe83b86a1724dc071e2489c6cdf0
Arash <arash.golrokhian@hotmail.com>
2022-03-09 14:17:06 +0330

    added Filter Module

--- store/views.py
+++ store/views.py
@@ -3,18 +3,22 @@ from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.views import APIView
 from rest_framework.mixins import ListModelMixin
 from rest_framework.viewsets import ModelViewSet
-from .serializers import ProductSerializer, CollectionSerializer
-from .models import Product, Collection, OrderItem
+from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
+from .models import Product, Collection, OrderItem, Review
+from .filter import ProductFilter
 
 
 # Product
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {'request': self.request}
@@ -25,9 +29,8 @@ class ProductViewSet(ModelViewSet):
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
 
+
 # Collection
-
-
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(
         product_count=Count('products')).all()
@@ -38,3 +41,14 @@ class CollectionViewSet(ModelViewSet):
             return Response({'This collection can not be deleted because it has more than one products in it'},
                             status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
+
+
+# Review
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
